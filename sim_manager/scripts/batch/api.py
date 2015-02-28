5effd40f55414dc67dfbb35f30d9d469873ef4a3
Jimm Domingo <jdomingo@green-code.com>
2015-02-28 15:32:08 -0600

    Added comment headers to all the source files that explain where the project's copyright and licensing information is located.

--- sim_manager/scripts/batch/api.py
+++ sim_manager/scripts/batch/api.py
@@ -1,3 +1,12 @@
+# This file is part of the Simulation Manager project for VecNet.
+# For copyright and licensing information about this project, see the
+# NOTICE.txt and LICENSE.md files in its top-level directory; they are
+# available at https://github.com/vecnet/simulation-manager
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License (MPL), version 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from abc import ABCMeta, abstractmethod
 
 
