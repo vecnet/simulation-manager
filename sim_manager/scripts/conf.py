601e18a3010f3eca163fbd550f2862fbc58e8ccb
Rob Hostetler <Macainian123@hotmail.com>
2015-06-24 17:39:37 -0400

    Fixed local models.

--- sim_manager/scripts/conf.py
+++ sim_manager/scripts/conf.py
@@ -39,16 +39,16 @@ if hostname in ('vecnet1', 'vecnet2', 'vecnet3', 'vecnet4'):  # JCU cluster
     BATCH_SYSTEM = batch.PBS
 
 """
-# Example of conf_local2.py file
+# Example of conf_local.py file
 from models import Mock, openmalaria
 MODELS = [
    openmalaria.SimulationModel('32', 'D:\\bin\\OM\\32\\openMalaria')
 ]
 """
 
-# try:
-#     from .conf_local import MODELS as LOCAL_MODELS
-#     MODELS += LOCAL_MODELS
-# except ImportError:
-#     LOCAL_MODELS = []  # To satisfy PyCharm code inspector
-#     pass
+try:
+    from conf_local import MODELS as LOCAL_MODELS
+    MODELS += LOCAL_MODELS
+except ImportError:
+    LOCAL_MODELS = []  # To satisfy PyCharm code inspector
+    pass
